e1f05f863cab57b1abbe844016ae8497805e9c1c
akatsukinoyami <akatsukinoyami@yahoo.com>
2021-10-20 19:49:54 +0300

    one more commit

--- common/aliases.nix
+++ common/aliases.nix
@@ -6,10 +6,11 @@
 			ext-gpu				= "env DRI_PRIME=1";
 
 			displaylink			= "cd ~/Downloads/ && nix-prefetch-url file://$PWD/displaylink.zip";
-			boot-nixos			= "displaylink && sudo nixos-rebuild boot --upgrade && tsu-push-conf";
-			switch-nixos		= "displaylink && sudo nixos-rebuild switch --upgrade && tsu-push-conf";
-			switch-all			= "displaylink && sudo nixos-rebuild switch --upgrade && home-manager switch && tsu-push-conf";
-			switch-home			= "home-manager switch && tsu-push-conf";
+			rebuild-nixos		= "displaylink && sudo nixos-rebuild";
+			boot-nixos			= "rebuild-nixos boot --upgrade && nixos-config-push";
+			switch-nixos		= "rebuild-nixos switch --upgrade && nixos-config-push";
+			switch-all			= "rebuild-nixos switch --upgrade && home-manager switch && nixos-config-push";
+			switch-home			= "home-manager switch && nixos-config-push";
 
 			nixos-config		= "code ~/nixos-config";
 			code-bots			= "code ~/Documents/katsu_bots";
@@ -17,14 +18,7 @@
 
 			nixos-clean			= "sudo nix-collect-garbage --delete-older-than 7d";
 			nixos-config-push	= "cd ~/nixos-config && git add * && git commit -m 'one more commit' && git push -u origin master";
-			
-			python-run			= "python-with discord flask pyrogram requests tgcrypto youtube-dl";
-			python				= "python-run python3";
-			python-shell		= "python-run fish";
 
-			youtube-shell		= "python-with youtube-dl";
-			youtube				= "youtube-shell 'youtube-dl '";
-			youtube-mp3			= "youtube-shell 'youtube-dl --extract-audio --audio-format mp3 '";
 			youtube-asmr		= "cd '/home/katsu/Music/asmr/' &&	youtube-mp3";
 			youtube-ost			= "cd '/home/katsu/Music/OST Vocal/' && youtube-both";
 
--- common/fish/functions/fish_prompt.fish
+++ common/fish/functions/fish_prompt.fish
@@ -5,10 +5,29 @@ function tsu-deploy
 	firebase deploy
 end
 
+function python-run			
+	nix-shell -p python38.withPackages(ps: with ps; [ discord flask pyrogram requests tgcrypto youtube-dl ]) --run $argv
+end
+function python				
+	python-run 'python3' $argv;
+end
+function python-shell		
+	python-run 'fish';
+end
 function python-with
-	nix-shell -p python38.withPackages(ps: with ps; [ $argv ]) --run 
+	nix-shell -p python38.withPackages(ps: with ps; [ $argv ]) --run 'fish'
 end
 
+function youtube-shell
+	echo $argv
+	nix-shell -p python38.withPackages(ps: with ps; [ youtube-dl ]) --run $argv
+end
+function youtube	
+	youtube-shell 'youtube-dl' \"$argv\"
+end
+function youtube-mp3
+	youtube-shell 'youtube-dl --extract-audio --audio-format mp3' \"$argv\"
+end
 function youtube-both
 	youtube-mp3 $argv
 	youtube $argv
@@ -16,48 +35,48 @@ end
 
 set python_libraries 'discord flask pyrogram requests tgcrypto youtube-dl'
 
-function tsu-help
+function alias-help
 	switch $argv
 		case 'pls'
-			set q '\033[37mAlias for sudo'
+			set q '\033[37mAlias for sudo.'
 		case 'ext-gpu'
 			set q '\033[37mAlias for executing application with nVidia GPU.'
-		case 'tsu-help'
-			set q '\033[37mPrint this message'
+		case 'alias-help'
+			set q '\033[37mPrint this message.'
 		case 'boot-nixos'
-			set q '\033[32mAlias for nixos-rebuild with reboot'
+			set q '\033[32mAlias for nixos-rebuild with reboot.'
 		case 'switch-all'
-			set q '\033[32mAlias for nixos-rebuild w/o reboot and home-manager switch and push config to github'
+			set q '\033[32mAlias for nixos-rebuild w/o reboot and home-manager switch and push config to github.'
 		case 'switch-nixos'
-			set q '\033[32mAlias for nixos-rebuild w/o reboot and push config to github'
+			set q '\033[32mAlias for nixos-rebuild w/o reboot and push config to github.'
 		case 'switch-home'
-			set q '\033[32mAlias for home-manager switch and push config to github'
+			set q '\033[32mAlias for home-manager switch and push config to github.'
 		case 'nixos-clean'
-			set q '\033[32mAlias for clean nixos-collect-garbage'
+			set q '\033[32mAlias for clean nixos-collect-garbage.'
 		case 'nixos-config-push'
-			set q '\033[32mAlias for making commit of nixos-config and push to github'
+			set q '\033[32mAlias for making commit of nixos-config and push to github.'
 		case 'nixos-config'
-			set q '\033[36mAlias for open VSCode with nixos-config folder'
+			set q '\033[36mAlias for open VSCode with nixos-config folder.'
 		case 'code-bots'
-			set q '\033[36mAlias for open VSCode with python-bots folder'
+			set q '\033[36mAlias for open VSCode with python-bots folder.'
 		case 'code-work'
-			set q '\033[36mAlias for open VSCode with work folder'
+			set q '\033[36mAlias for open VSCode with work folder.'
 		case 'python-shell'
-			set q '\033[33mAlias for open shell with python and pre-defined libraries\nWrite tsu-help python-libraries to print pre-defined libraries.'
+			set q '\033[33mAlias for open shell with python and pre-defined libraries\nWrite tsu-help python-libraries to print pre-defined libraries..'
 		case 'python'
 			set q '\033[33mAlias for open (if) specified script with python and pre-defined libraries (else) python interpreter.\nWrite tsu-help python-libraries to print pre-defined libraries.'
 		case 'python-libraries'
-			set q "\033[33mPre-defined libraries for python: $python_libraries"
+			set q "\033[33mPre-defined libraries for python: $python_libraries."
 		case 'youtube'
-			set q '\033[31mAlias for download video from youtube by specified link'
+			set q '\033[31mAlias for download video from youtube by specified link.'
 		case 'youtube-mp3'
-			set q '\033[31mAlias for download audio from youtube by specified link'
+			set q '\033[31mAlias for download audio from youtube by specified link.'
 		case 'youtube-both'
-			set q '\033[31mAlias for download audio and video from youtube by specified link'
+			set q '\033[31mAlias for download audio and video from youtube by specified link.'
 		case 'youtube-asmr'
-			set q '\033[31mAlias for change directory to Music/ASMR and download audio from youtube by specified link'
+			set q '\033[31mAlias for change directory to Music/ASMR and download audio from youtube by specified link.'
 		case 'youtube-ost'
-			set q '\033[31mAlias for change directory to Music/OST and download audio and video from youtube by specified link'
+			set q '\033[31mAlias for change directory to Music/OST and download audio and video from youtube by specified link.'
 		case '*'
 			set q 'Aliases in system:\n
   \033[32mFor NixOS: boot-nixos, switch-nixos, switch-all, switch-home, nixos-clean, nixos-config-push;
@@ -65,7 +84,7 @@ function tsu-help
   \033[33mFor Python: python, python-run, python-shell;
   \033[31mFor youtube-dl: youtube, youtube-mp3, youtube-both, youtube-asmr, youtube-ost;
   \033[37mOther: pls, ext-gpu, tsu-help.
-\n\033[0mWrite tsu-help /some-alias/ for info about it.'
+\n\033[0mWrite alias-help /some-alias/ for info about it.'
 	end
 	echo -e $q
 end
